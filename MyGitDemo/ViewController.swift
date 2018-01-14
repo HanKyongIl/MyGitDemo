76590c56ce6d18fce05420e230e78471048fe3aa
Han <han@hans-mac.local>
2018-01-14 09:57:13 -0800

    A property for sum was declared. The first addition was made and logged to the debugger.

--- MyGitDemo/ViewController.swift
+++ MyGitDemo/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var sum:Int?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +19,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        let a = 5
+        let b = 6
+        sum = a + b
+        print("The result is:\(String(describing: sum))")
     }
 
 
