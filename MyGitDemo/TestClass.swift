05860f754405c3344703550ef52065a52c7ab838
Han <han@hans-mac.local>
2018-01-14 11:56:10 -0800

    Added a new class named TestClass

---
+++ MyGitDemo/TestClass.swift
@@ -0,0 +1,13 @@
+//
+//  TestClass.swift
+//  MyGitDemo
+//
+//  Created by Han on 1/14/18.
+//  Copyright © 2018 Han. All rights reserved.
+//
+
+import UIKit
+
+class TestClass: NSObject {
+    var value:Int?
+}
